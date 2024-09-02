8d2cd84bb675d6d51227c29965b4cd25d0994f6a
HUSEIN ZOLKEPLI <husein.zol05@gmail.com>
2024-09-02 09:16:51 +0800

    added nodejs example

--- cookbook/speech/websocket/README.md
+++ cookbook/speech/websocket/README.md
@@ -14,10 +14,10 @@ https://github.com/user-attachments/assets/2f10d2c9-90b8-4561-97e5-872469732ba1
 
 [recordrtc-fastapi.py](recordrtc-fastapi.py).
 
-1. Basically, Frontend RecordRTC JS -> (Websocket) Local Backend -> (Websocket) Websocket Speech API.
+1. Basically, Frontend RecordRTC JS -> (Websocket) FastAPI -> (Websocket) Websocket Speech API.
 2. The reason why you cannot call directly from Frontend is to prevent API key leakage to the users.
 
-## how to run
+### how to run
 
 1. Run FastAPI,
 
@@ -28,3 +28,17 @@ uvicorn recordrtc-fastapi:app --reload --host 0.0.0.0 --port 8000
 2. Access http://localhost:8000,
 
 https://github.com/user-attachments/assets/ac46fede-f6fe-43de-8333-f0b0f8f04bd3
+
+## RecordRTC JS -> Websocket ExpressJS
+
+1. Basically, Frontend RecordRTC JS -> (Websocket) ExpressJS -> (Websocket) Websocket Speech API.
+2. The reason why you cannot call directly from Frontend is to prevent API key leakage to the users.
+
+### how to run
+
+1. Run Node JS,
+
+```bash
+npm install express ws
+node recordrtc.js
+```
---
+++ cookbook/speech/websocket/recordrtc.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const url = require('url');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ noServer: true });
+
+const apikey = '';
+const base_url = "wss://llm-router.nous.mesolitica.com/audio/transcriptions/ws";
+
+// Check parameter at https://llm-router.nous.mesolitica.com/scalar#tag/audio-transcription/POST/audio/transcriptions
+const params = new URLSearchParams({
+    "apikey": apikey,
+    "language": "en"
+});
+
+const uri = `${base_url}?${params}`;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.on('upgrade', (request, socket, head) => {
+    const pathname = url.parse(request.url).pathname;
+
+    if (pathname.startsWith('/ws/')) {
+        const client_id = pathname.split('/')[2];
+
+        wss.handleUpgrade(request, socket, head, (ws) => {
+            wss.emit('connection', ws, request, client_id);
+        });
+    } else {
+        socket.destroy();
+    }
+});
+
+wss.on('connection', (ws, req) => {
+    const websocket_api = new WebSocket(uri);
+
+    ws.on('message', async (message) => {
+        websocket_api.send(message.toString());
+    });
+
+    websocket_api.on('message', (message) => {
+        ws.send(message.toString());
+    });
+
+    ws.on('close', () => {
+        websocket_api.close();
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
